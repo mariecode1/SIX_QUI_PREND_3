fabafa89c4067440956402ff90de7828b866c6bd
mariecode1 <kairouzmariee@gmail.com>
2023-05-31 13:51:50 +0200

    CONSOLE

--- src/main/java/com/example/fx/mechanic/Start.java
+++ src/main/java/com/example/fx/mechanic/Start.java
@@ -20,43 +20,44 @@ import static com.example.fx.mechanic.turn.game;
 import static com.example.fx.object.Card.cartes;
 
 public class Start {
-    private static Stage primaryStage;
-    public static int ia;
-
-    public Start(Stage primaryStage) {
-        this.primaryStage = primaryStage;
-    }
-
-    public static void regle() {
+    public static void regle(){
         method.printTitle("Regle du jeu :");
-        String regles =
+        System.out.println(
                 "Nombre de joueurs : 2 à 10 joueurs\n" +
                         "\n" +
                         "But du jeu : Éviter de ramasser des têtes de bœufs (représentées par des cartes) qui ont une valeur en points. Le joueur avec le moins de points à la fin de la partie gagne.\n" +
                         "\n" +
                         "Déroulement du jeu :\n" +
                         "\n" +
-                        "1. Distribuez 10 cartes à chaque joueur.\n" +
-                        "2. Placez quatre cartes au centre de la table pour commencer le jeu.\n" +
-                        "3. Les joueurs choisissent une carte de leur main et la placent face cachée sur la table.\n" +
-                        "4. Lorsque tous les joueurs ont choisi leur carte, ils la révèlent simultanément.\n" +
-                        "5. Les cartes sont alors placées sur la table en ordre croissant (de la plus petite à la plus grande) en respectant les règles suivantes :\n\n" +
-                        "   - Les cartes doivent être placées sur la même ligne que la carte qui a la plus petite valeur et doit être inférieure à la carte la plus grande.\n\n" +
-                        "   - Si une carte est supérieure à toutes les cartes sur la table, elle doit commencer une nouvelle ligne.\n\n" +
-                        "   - Si une carte est égale à une ou plusieurs cartes sur la table, elle doit être placée à droite de ces cartes.\n\n" +
-                        "6. Si un joueur doit placer sa carte sur une ligne qui a déjà six cartes, il doit prendre la ligne complète et toutes les cartes de la ligne sont ajoutées à sa pile de têtes de bœufs.\n" +
-                        "7. Les joueurs répètent ce processus pour chaque tour jusqu'à ce que toutes les cartes de la main de chaque joueur soient jouées.\n" +
-                        "8. La partie se termine lorsque la dernière carte est jouée. Les joueurs comptent alors les points de leurs têtes de bœufs. Chaque tête de bœuf vaut de 1 à 7 points, selon le numéro de la carte. Le joueur avec le moins de points gagne la partie.\n\n";
-
+                        "1 . Distribuez 10 cartes à chaque joueur.\n" +
+                        "2 . Placez quatre cartes au centre de la table pour commencer le jeu.\n" +
+                        "3 . Les joueurs choisissent une carte de leur main et la placent face cachée sur la table.\n" +
+                        "4 . Lorsque tous les joueurs ont choisi leur carte, ils la révèlent simultanément.\n" +
+                        "5 . Les cartes sont alors placées sur la table en ordre croissant (de la plus petite à la plus grande) en respectant les règles suivantes :\n" +
+                        "\n\n     - Les cartes doivent être placées sur la même ligne que la carte qui a la plus petite valeur et doit être inférieure à la carte la plus grande.\n" +
+                        "\n     - Si une carte est supérieure à toutes les cartes sur la table, elle doit commencer une nouvelle ligne.\n" +
+                        "\n     - Si une carte est égale à une ou plusieurs cartes sur la table, elle doit être placée à droite de ces cartes.\n\n" +
+                        "6 . Si un joueur doit placer sa carte sur une ligne qui a déjà six cartes, il doit prendre la ligne complète et toutes les cartes de la ligne sont ajoutées à sa pile de têtes de bœufs.\n" +
+                        "7 . Les joueurs répètent ce processus pour chaque tour jusqu'à ce que toutes les cartes de la main de chaque joueur soient jouées.\n" +
+                        "8 . La partie se termine lorsque la dernière carte est jouée. Les joueurs comptent alors les points de leurs têtes de bœufs. Chaque tête de bœuf vaut de 1 à 7 points, selon le numéro de la carte. Le joueur avec le moins de points gagne la partie. \n\n");
         method.enterContinue();
         method.clearConsole();
     }
 
-    public static void start() {
+    public static void start(){
+
         Random random = new Random();
         Card.cart();
         // Mélanger les cartes
-        Collections.shuffle(cartes);
+        Collections.shuffle(cartes);//MELANGER
+
+        System.out.println("combien de joueur etes vous :");
+        int nbr_joueur= sc.nextInt();
+        System.out.println("Voulez-vous ajouter une IA :" +
+                "1. yes       2. no");
+        int ia=method.scInt("->",2);
+
+
         // Distribuer les cartes aux joueurs
         for (int i = 0; i < nbr_joueur; i++) {
             main = new ArrayList<>();
@@ -65,15 +66,15 @@ public class Start {
             joueursPli.add(bin);
             AI.joueursPliV.add(AI.binV);
         }
-        if (ia == 1) {
+        if (ia==1){
             joueurs.add(AI.ordimain);
             joueursPli.add(AI.ordibin);
             AI.joueursPliV.add(AI.ordibinV);
         }
 
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 10; j++) {// DISTRIBUTION
             method.printLine(20);
-            for (int i = 0; i <= joueurs.size() - 1; i++) {
+            for (int i = 0; i <=joueurs.size()-1; i++) {
                 joueurs.get(i).add(cartes.get(0));
                 cartes.remove(cartes.get(0));
                 show(i);
@@ -87,44 +88,33 @@ public class Start {
             method.clearConsole();
         }
         AI.carteRest();
-        for (int i = 0; i < joueurs.size(); i++) {
+        for (int i = 0; i <joueurs.size() ; i++) {
             show(i);
         }
         System.out.println();
-    }
 
-    public static void GameLogic() {
-        Random random = new Random();
+    }
+    public static void GameLogic(){
         // Jouer au  jeu
         init();
-        while (joueurs.get(joueurs.size()).size() != 0) {
+        while (joueurs.get(joueurs.size()-1).size()!=0) {
             game();
         }
-        List<Integer> scores = new ArrayList<>();
-        for (int i = 0; i < joueurs.size(); i++) {
+        List<Integer> Score = new ArrayList<>();;
+        for (int i = 0;i<joueurs.size();i++){
             int point = 0;
-            for (int j = 0; j < joueursPli.get(i).size(); j++) {
+            for (int j = 0; j<joueursPli.get(i).size();j++){
                 point += joueursPli.get(i).get(j).getNbrTaureau();
             }
-            scores.add(point);
-            System.out.println("Nombre de taureaux pour le joueur " + i + " : " + point);
+            Score.add(point);
+            System.out.println("Nombre de taureau joueurs " + i + " : " + point);
         }
         method.enterContinue();
         method.clearConsole();
         method.printLine(40);
-        afficherElementPlusPetit(scores);
-        afficherElementPlusGrand(scores);
+        afficherElementPlusPetit(Score);
+        afficherElementPlusGrand(Score);
         method.printLine(40);
-    }
 
-    private static void showAlert(String message) {
-        Platform.runLater(() -> {
-            Alert alert = new Alert(AlertType.INFORMATION);
-            alert.initOwner(primaryStage);
-            alert.setTitle("Regle du jeu");
-            alert.setHeaderText(null);
-            alert.setContentText(message);
-            alert.showAndWait();
-        });
     }
 }
